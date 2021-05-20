2e07a9affa6e3c35a11ecf71506cb1d0abf6b656
Taswen <void_Liberty@hotmail.com>
2021-05-21 02:10:11 +0800

    添加分页

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from ext import db
-from db import get_data
+from db import *
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile("settings.py")
+    app.config.from_pyfile("config/settings.py")
     db.init_app(app)
     return app
 
@@ -14,9 +14,17 @@ app = create_app()
 
 
 @app.route('/')
-def hello_world():
-    earthquakes = get_data()
-    return render_template("index.html", earthquakes=earthquakes)
+def earthquakesListPage():
+
+
+    result = request.args.get("result", 'ALL', str)
+    offset = request.args.get('offset', 0, int)
+    limit = request.args.get('limit', 20, int)
+    count = get_earthquakes_num()
+    earthquakes = get_earthquakes_data(limit=limit,offset=offset)
+
+
+    return render_template("index.html",count=count, result=result, earthquakes=earthquakes,offset=offset,limit = limit)
 
 
 
