6f778adca8cb0a6c301a176dbeac3a85382962e8
Taswen <void_Liberty@hotmail.com>
2021-05-21 03:52:13 +0800

    添加上传功能

--- app/app.py
+++ app/app.py
@@ -1,21 +1,21 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect
+from flask.helpers import flash, url_for
 from app.ext import db
 from app.db import *
+import os
 
-
+############# INIT ##############
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile("config/settings.py")
     db.init_app(app)
     return app
 
-
 app = create_app()
 
-
+############# ROUTE ##############
 @app.route('/')
 def earthquakesListPage():
-
 
     result = request.args.get("result", 'ALL', str)
     offset = request.args.get('offset', 0, int)
@@ -23,11 +23,21 @@ def earthquakesListPage():
     count = get_earthquakes_num()
     earthquakes = get_earthquakes_data(limit=limit,offset=offset)
 
-
     return render_template("index.html",count=count, result=result, earthquakes=earthquakes,offset=offset,limit = limit)
 
+@app.route('/uploader',methods=['POST','GET'])
+def uploader():
+    if request.method == 'POST':
+        print("IN")
+        f = request.files['file']
+        print(os.path)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))        
+        flash('file uploaded successfully')
+    else:
+        print("IN GET")
+    return redirect(url_for("earthquakesListPage"))
 
 
-
+#############  ##############
 if __name__ == '__main__':
     app.run(debug=True)
--- app/config/settings.py
+++ app/config/settings.py
@@ -1,5 +1,6 @@
 debug=True
-
+SECRET_KEY="=kls2kC3bs4ABbc"
+UPLOAD_FOLDER='uploadFiles/backup'
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://nero:Buptsse18.@47.93.229.92:3306/data?charset=utf8mb4"#
 
 SQLALCHEMY_TRACK_MODIFICATIONS=True
