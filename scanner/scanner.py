1f3809b0a8c89b1f516d0cee04c848bef94105f4
nero <tianhp00@hotmail.com>
2021-05-25 18:34:40 +0800

    [feat] support json files

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -4,64 +4,77 @@ import csv
 import threading
 import time
 import schedule
-from typing import Dict
+import json
+from typing import Dict, List
 from flask_sqlalchemy import SQLAlchemy
 
 from app.models import Disaster, Earthquake
+from app.config.settings import UPLOAD_FOLDER, SCANNED_FOLDER, ERROR_FOLDER
+from validator.earthquake_validator import EarthquakeValidator
+
+
+def json_reader(path: str) -> List[Dict]:
+    with open(path, "r") as file:
+        return json.loads(''.join(file.readlines()))['disasters']
+
+
+def csv_reader(path: str) -> List[Dict]:
+    with open(path, "r") as file:
+        res = []
+        reader = csv.DictReader(file)
+        for row in reader:
+            res.append(row)
+        return res
 
 
 class Scanner(threading.Thread):
-    upload_path = '/home/dataput/mshd/upload/'
-    scanned_path = '/home/dataput/mshd/scanned/'
-    error_path = '/home/dataput/mshd/error/'
-
     def __init__(self, app):
         super().__init__()
         self.app = app
         app.app_context().push()
         self.session = SQLAlchemy().session
-
-    @classmethod
-    def valid_row(cls, row: Dict) -> bool:
-        earthquake_minimal_attr = ['Longitude', 'Latitude', 'Level']
-        for attr in earthquake_minimal_attr:
-            if not row.get(attr, ''):
-                return False
-        return True
+        self.type_func_map = {
+            'csv': csv_reader,
+            'json': json_reader,
+        }
+        self.validator = EarthquakeValidator()
 
     def start_reading(self):
         print("start reading")
-        for filename in os.listdir(self.upload_path):
+        for filename in os.listdir(UPLOAD_FOLDER):
             try:
-                with open(os.path.join(self.upload_path, filename), "r") as file:
-                    valid_row = 0
-                    reader = csv.DictReader(file)
-                    for row in reader:
-                        if not self.valid_row(row):
-                            logging.warning(f"Ignoring row {row}.")
-                            continue
-                        disaster = Disaster()
-                        disaster.TypeCode = 1
-                        self.session.add(disaster)
-                        self.session.flush()
-                        earthquake = Earthquake(row)
-                        earthquake.ReferenceId = disaster.Id
-                        self.session.add(earthquake)
-                        self.session.commit()
-                        valid_row += 1
-                    if valid_row == 0:
-                        raise Exception('Too little valid rows.')
-                os.rename(os.path.join(self.upload_path, filename),
-                          os.path.join(self.scanned_path, filename))
+                ext_name = filename.rsplit('.', maxsplit=1)
+                if len(ext_name) < 2 or ext_name[1] not in self.type_func_map:
+                    raise TypeError('Not a accepted file type')
+                func = self.type_func_map[ext_name[1]]
+                disaster_list = func(os.path.join(UPLOAD_FOLDER, filename))
+                valid_row_count = 0
+                for disaster_dict in disaster_list:
+                    if not self.validator.validate(disaster_dict):
+                        logging.warning(f"Ignoring row {disaster_dict}.")
+                        continue
+                    disaster = Disaster()
+                    disaster.TypeCode = 1
+                    self.session.add(disaster)
+                    self.session.flush()
+                    earthquake = Earthquake(disaster_dict)
+                    earthquake.ReferenceId = disaster.Id
+                    self.session.add(earthquake)
+                    valid_row_count += 1
+                if valid_row_count == 0:
+                    raise Exception('Too little valid rows.')
+                self.session.commit()
+                os.rename(os.path.join(UPLOAD_FOLDER, filename),
+                          os.path.join(SCANNED_FOLDER, filename))
             except Exception:
                 logging.exception(f"error while processing {filename}:")
                 self.session.rollback()
-                os.rename(os.path.join(self.upload_path, filename),
-                          os.path.join(self.error_path, filename))
+                os.rename(os.path.join(UPLOAD_FOLDER, filename),
+                          os.path.join(ERROR_FOLDER, filename))
 
     @classmethod
     def init_path(cls) -> None:
-        for path in [cls.upload_path, cls.scanned_path, cls.error_path]:
+        for path in [ERROR_FOLDER, UPLOAD_FOLDER, SCANNED_FOLDER]:
             os.makedirs(path, exist_ok=True)
 
     def run(self) -> None:
